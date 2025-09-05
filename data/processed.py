b4fd69e74f9b1e90af7cebd2afa02b1ae1eca72a
Solomon Sannie <solosannie@gmail.com>
2025-09-05 17:32:29 +0000

    Update and rename processed to processed.py

---
+++ data/processed.py
@@ -0,0 +1,4 @@
+events_url = "https://drive.google.com/uc?id=1pgf3rudMhoaOGB1MeIe41NIulxPq8okd"
+item_props_url = "https://drive.google.com/uc?id=1PveWfRDtgg5ytXdepZm2l_50n_quWbbB"
+category_tree_url = "https://drive.google.com/uc?id=1dZ1eAvcPDY3RSuPBCezW9-WSRrT5uYSy"
+user_features_url = ""
