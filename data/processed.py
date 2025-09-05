2686195c6aefad14e97c85925fff0e14f8ed6f83
Solomon Sannie <solosannie@gmail.com>
2025-09-05 18:00:41 +0000

    Update processed.py

--- data/processed.py
+++ data/processed.py
@@ -1,4 +1,8 @@
-events_url = "https://drive.google.com/uc?id=1pgf3rudMhoaOGB1MeIe41NIulxPq8okd"
-item_props_url = "https://drive.google.com/uc?id=1PveWfRDtgg5ytXdepZm2l_50n_quWbbB"
+## Download the following Datasets using the embedded links
+
+events_filtered_url = "https://drive.google.com/uc?id=1pgf3rudMhoaOGB1MeIe41NIulxPq8okd"
+item_props_filtered_url = "https://drive.google.com/uc?id=1PveWfRDtgg5ytXdepZm2l_50n_quWbbB"
 category_tree_url = "https://drive.google.com/uc?id=1dZ1eAvcPDY3RSuPBCezW9-WSRrT5uYSy"
-user_features_url = ""
+user_features_url = "https://drive.google.com/uc?id=1OIltuPKezK0485AhwGs9BOjna6BcTK8s"
+predicted_values(X_Test)_url = "https://drive.google.com/uc?id=1Bv1scxsVXQGKycXz_o6zBBPO2OwKnBoT"
+merged_data_sample = "https://drive.google.com/uc?id=1GUVlXmn8Lv3KL8gU3eE2UmgAKdpRmbym"
